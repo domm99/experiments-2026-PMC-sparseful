fix: remove softmax

src/learning/model.py
@@ -12,4 +12,4 @@ class MLP(nn.Module):
         x = x.view(-1, 28 * 28)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        return x